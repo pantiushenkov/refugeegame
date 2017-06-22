0b01e4bca56885907b417b10e0283187f4182e4e
slavikgav <gavryshko.sl@gmail.com>
2017-06-22 13:57:16 +0300

    Customs scene started,falling objects added,death zone created

---
+++ Assets/Scripts/FallingObjects.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjects : MonoBehaviour {
+
+    public List<GameObject> fallingObjects;
+    int min = -5;
+    int max = 5;
+    int numberOfObjects = 4;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("createRandomObject", 0.0f, 1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      
+    }
+
+    Vector3 GeneratedPosition()
+    {
+        int x, y, z;
+        x = Random.Range(min, max);
+        y = 5;
+        z = 0;
+        return new Vector3(x, y, z);
+    }
+
+    void createRandomObject()
+    {
+        Instantiate(fallingObjects[Random.Range(0,4)], GeneratedPosition(), Quaternion.identity);
+    }
+
+    IEnumerator delay()
+    {
+        Debug.Log("Waiting...");
+        yield return new WaitForSeconds(9);   
+    }
+}
---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour {
+
+    private void Start()
+    {
+        Debug.Log("POS X: "+ this.transform.position.x);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Destroy(this.gameObject);
+    }
+
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+        Destroy(this.gameObject);
+    }
+}
