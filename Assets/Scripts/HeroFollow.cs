d21e826a0d3948e1043fda0c75e04fe9f0974b37
VLad <pantiushenkov@gmail.com>
2017-06-22 13:20:27 +0300

    initial commit

---
+++ Assets/Scripts/HeroFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+	public HeroRefugee refugee;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+		Transform refugee_transform = refugee.transform;
+		Transform camera_transform = this.transform;
+		Vector3 refugee_position = refugee_transform.position;
+		Vector3 camera_position = camera_transform.position;
+		camera_position.x = refugee_position.x;
+		camera_position.y = refugee_position.y;
+		camera_transform.position = camera_position;
+	}
+}
---
+++ Assets/Scripts/HeroRefugee.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRefugee : MonoBehaviour {
+	
+	public float speed = 0.5f;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		float valueX = Input.GetAxis("Horizontal");
+		float valueY = Input.GetAxis("Vertical");
+		Vector3 moveDirection = new Vector3(valueX, valueY, 0.0f);
+
+		transform.position = transform.position + moveDirection * speed;
+		
+	}
+}
