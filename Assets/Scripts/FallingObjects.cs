abfc78b872fc17cd31e7289671d1bb7cd58f1bb0
slavikgav <gavryshko.sl@gmail.com>
2017-06-26 13:28:15 +0300

    Question pop-up added,Escape denied pop-up added,game play improved

--- Assets/Scripts/FallingObjects.cs
+++ Assets/Scripts/FallingObjects.cs
@@ -14,9 +14,14 @@ public class FallingObjects : MonoBehaviour {
     int obtainedObjects = 0;
     int objectsToPass = 30;
 
-	// Use this for initialization
-	void Start () {
+    bool play = true;
+
+    public GameObject escapeDeniedPrefab;
+
+    // Use this for initialization
+    void Start () {
         fallingObjectsInstance = this;
+        objectsLabel.text = 0+"/" + objectsToPass;
         InvokeRepeating("createRandomObject", 0.0f, 1f);
         Debug.Log("LIST: " );
     }
@@ -37,7 +42,8 @@ public class FallingObjects : MonoBehaviour {
 
     void createRandomObject()
     {
-        Instantiate(fallingObjects[Random.Range(0,5)], GeneratedPosition(), Quaternion.identity);
+        if(play)
+            Instantiate(fallingObjects[Random.Range(0,5)], GeneratedPosition(), Quaternion.identity);
     }
 
     IEnumerator delay()
@@ -51,13 +57,23 @@ public class FallingObjects : MonoBehaviour {
         if (fallingObject is Weapon)
         {
             Debug.Log("BADDDDD BOY");
+            play = false;
+            showEscapeDeniedPopUp();
         }
         else {
             obtainedObjects += 1;
             objectsLabel.text = obtainedObjects + "/" + objectsToPass;
 
         }
+    }
 
-   
+    void showEscapeDeniedPopUp()
+    {
+        //Do something
+        GameObject parent = UICamera.first.transform.parent.gameObject;
+        Debug.Log("PARENT NAME: " + parent.name);
+        //Prefab
+        GameObject obj = NGUITools.AddChild(parent, escapeDeniedPrefab);
+        EscapeDeniedPopUp popup = obj.GetComponent<EscapeDeniedPopUp>();
     }
 }
